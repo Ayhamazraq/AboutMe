9610bd26c54a41b39b5c6e550dd2710b530f0389
Ayhamazraq <ayham.bluee@gmail.com>
2021-08-02 16:56:01 +0300

    add new file

---
+++ js/app.js
@@ -0,0 +1,53 @@
+var namee = prompt("what is your name")
+alert("hey, wlecome to my website " + namee)
+
+ var talk = prompt("so do you working? plese answer yes or no").toLowerCase();
+
+ if (talk == "yes" || talk == "y") {
+   
+     alert("good job man")
+
+}else if (talk == "n" ||tlak=="no") {
+        alert("im sorry man")
+        
+    }
+
+     var eat = prompt("so tell me do you love italian food? plese answer yes or no").toLowerCase();
+     if (eat == "yes" || eat == "y") {
+        var eat = prompt("so do you love pizza or pasta ? ").toLowerCase();
+   
+         alert("good same man")
+    
+    }else if (eat == "n" ||eat=="no") {
+        
+             alert("mm same me")
+            
+        }
+
+
+
+     var hob = prompt("do you love football? plese answer yes or no").toLowerCase();
+     if (hob == "yes" || hob == "y") {
+
+   
+        alert("good same me man")
+    
+     }else if (hob == "n" ||hob=="no") {
+        
+             alert(" same me")
+            
+       }
+
+
+    var mus = prompt("do you love music? plese answer yes or no").toLowerCase();
+    if (mus == "yes" || mus == "y") {
+
+   
+        alert("good same me man")
+    
+    }else if (mus == "n" ||mus=="no") {
+        
+            alert(" same me")
+            
+        }
+
